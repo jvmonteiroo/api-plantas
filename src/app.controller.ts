fee2ef1254a19c662a804b94166c17f74ba5d6c5
João Vitor <joaovitor0928@hotmail.com>
2024-01-30 19:49:28 -0300

    integrei com o codigo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,9 +5,48 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): object {
-    return this.appService.getHello();
+  @Get("plant-types")
+  getPlantTypes(): object {
+    return [
+      {
+        name: "Imbé",
+        image:"imbe",
+      },
+      {
+        name: "Peperomia",
+        image: "peperomia",
+      },
+      {
+        name: "Aningapara",
+        image: "aningapara",
+      },
+      {
+        name: "Yucca",
+        image:"yucca",
+      },
+      {
+        name: "Espada de São Jorge",
+        image: "espada",
+      },
+      {
+        name: "Zamioculca",
+        image: "zamioculca",
+      },
+    ]
+  }
+
+  @Get("plant-types/:id")
+  getById()
+  {
+    return {
+      name: "Zamioculca",
+      image: "zamioculca",
+    }
+  }
+
+  @Get("plant-places")
+  getPlantPlaces(){
+    return ["Sala", "Cozinha", "Garagem", "Quarto", "Sala2"]
   }
 
   @Post()
